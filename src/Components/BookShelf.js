be019e2b6b9f21faf775c209921b2f31a7c83b77
Manh Le <quang.manh@gmail.com>
2017-11-04 12:52:32 -0500

    Add contents to compoments

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,11 +1,25 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Book from './Book'
 
 class BookShelf extends Component {
-    render(){
+
+    render() {
+        const { books } = this.props
+
         return (
-            
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {
+                            books.map(b => <li><Book/></li>)
+                        }
+                    </ol>
+                </div>
+            </div>
         )
     }
 }
+
+export default BookShelf
---
+++ src/Components/SearchPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class SearchPage extends Component {
+    render(){
+        
+    }
+}
