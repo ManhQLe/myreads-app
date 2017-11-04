8b533203d7fe7264491b4e0381fa4a697e4ded16
Manh Le <quang.manh@gmail.com>
2017-11-04 12:24:51 -0500

    Add components, create temp folder

---
+++ src/Components/BookShelf.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+import Book from './Book'
+
+class BookShelf extends Component {
+    render(){
+        return (
+            
+        )
+    }
+}
