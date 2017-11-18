bc9d5d1efe2bd4f608e59a9bed2a1dfa1bdb4041
Manh Le <quang.manh@gmail.com>
2017-11-17 20:46:38 -0600

    change SearchPage to functional

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 import { Debounce } from 'react-throttle'
 
-class SearchPage extends Component {
+function SearchPage(props) {
 
-    onShelfChanged = (book, shelfName) => {
-        const { onShelfChanged } = this.props;
+    const onShelfChanged = (book, shelfName) => {
+        const { onShelfChanged } = props;
         onShelfChanged && onShelfChanged(book, shelfName)
     }
 
-    render() {
-        const { words, found } = this.props.searchInfo;
+    const { words, found } = props.searchInfo;
 
-        return (
-            <div className="search-books">
-                {
-                    found && <h1 className="search-result-text">{`Found ${found.length} book${found.length ? 's' : ''}.`}</h1>
-                }
-                <div className="search-books-bar">
-                    <a className="close-search" onClick={() => window.history.back()}>Close</a>
-                    <div className="search-books-input-wrapper">
-                        <Debounce time="400" handler="onChange">
-                            <input type="text" placeholder="Search by title or author" defaultValue={words}
-                                onChange={(e) => this.props.onQueryChanged(e.target.value)} />
-                        </Debounce>
-                    </div>
-                </div>
-                <div className="search-books-results">
-                    <ol className="books-grid">
-                        {
-                            found && found.map(b =>
-                                <li key={b.id}>
-                                    <Book displayShelf onShelfChanged={this.onShelfChanged} book={b} />
-                                </li>
-                            )
-                        }
-                    </ol>
+    return (
+        <div className="search-books">
+            {
+                found && <h1 className="search-result-text">{`Found ${found.length} book${found.length ? 's' : ''}.`}</h1>
+            }
+            <div className="search-books-bar">
+                <a className="close-search" onClick={() => window.history.back()}>Close</a>
+                <div className="search-books-input-wrapper">
+                    <Debounce time="400" handler="onChange">
+                        <input type="text" placeholder="Search by title or author" defaultValue={words}
+                            onChange={(e) => props.onQueryChanged(e.target.value)} />
+                    </Debounce>
                 </div>
             </div>
-        )
-    }
+            <div className="search-books-results">
+                <ol className="books-grid">
+                    {
+                        found && found.map(b =>
+                            <li key={b.id}>
+                                <Book displayShelf onShelfChanged={onShelfChanged} book={b} />
+                            </li>
+                        )
+                    }
+                </ol>
+            </div>
+        </div>
+    )
+
 }
 
 export default SearchPage
