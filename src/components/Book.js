9bfcb487268074e678ca9324688ed617d0f7960e
Manh Le <quang.manh@gmail.com>
2017-11-17 20:14:56 -0600

    Convert Book to functional comp

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,51 +1,49 @@
-import React, { Component } from 'react'
+import React from 'react'
 import AppSettings from '../AppSettings'
 import { Link } from 'react-router-dom'
 
 
-class Book extends Component {
+function Book(props) {
 
-    static getAuthorName(book) {
-        return book.authors ? book.authors.join(", ") : ""
-    }
 
-    static getReviewCount(book){
-        return book.ratingsCount?book.ratingsCount:0
-    }
-
-    static getThumbnailUrl(book) {
-        const thumbnail = book.imageLinks ? book.imageLinks.thumbnail : null;
-        return thumbnail || "/icons/add.svg";
-    }
-
-    render() {
-        const { book,displayShelf } = this.props
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <Link to={`/book/${book.id}`}>
-                        <div className="book-cover"
-                            style={{ width: 128, height: 193, backgroundImage: `url("${Book.getThumbnailUrl(book)}")` }}></div>
-                    </Link>
-                    {
-                       displayShelf && book.shelf&& book.shelf!=='none' &&<div className="book-shelf-label">{AppSettings.getShelfName(book.shelf)}</div>
-                    }
-                    <div className="book-shelf-changer">
-                        <select onChange={(e) =>this.props.onShelfChanged(book, e.target.value)} 
+    const { book, displayShelf } = props
+    return (
+        <div className="book">
+            <div className="book-top">
+                <Link to={`/book/${book.id}`}>
+                    <div className="book-cover"
+                        style={{ width: 128, height: 193, backgroundImage: `url("${Book.getThumbnailUrl(book)}")` }}></div>
+                </Link>
+                {
+                    displayShelf && book.shelf && book.shelf !== 'none' && <div className="book-shelf-label">{AppSettings.getShelfName(book.shelf)}</div>
+                }
+                <div className="book-shelf-changer">
+                    <select onChange={(e) => this.props.onShelfChanged(book, e.target.value)}
                         defaultValue={book.shelf && book.shelf.length ? book.shelf : 'none'}>
-                            <option value="none" disabled>Move to...</option>
-                            {
-                                AppSettings.shelfOrderMap.map(sid => (
-                                    <option key={sid} value={sid}>{AppSettings.getShelfName(sid)}</option>
-                                ))
-                            }
-                        </select>
-                    </div>
+                        <option value="none" disabled>Move to...</option>
+                        {
+                            AppSettings.shelfOrderMap.map(sid => (
+                                <option key={sid} value={sid}>{AppSettings.getShelfName(sid)}</option>
+                            ))
+                        }
+                    </select>
                 </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{Book.getAuthorName(book)}</div>
-            </div>)
-    }
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{Book.getAuthorName(book)}</div>
+        </div>)
+
+}
+Book.getAuthorName=(book)=> {
+    return book.authors ? book.authors.join(", ") : ""
 }
 
+Book.getReviewCount=(book)=>{
+    return book.ratingsCount?book.ratingsCount:0
+}
+
+Book.getThumbnailUrl=(book)=> {
+    const thumbnail = book.imageLinks ? book.imageLinks.thumbnail : null;
+    return thumbnail || "/icons/add.svg";
+}
 export default Book;
