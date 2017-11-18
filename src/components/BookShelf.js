a0a69048b510f2d8e68a95b659379c8d920ad47b
Manh Le <quang.manh@gmail.com>
2017-11-17 20:07:50 -0600

    Change BookShelf to functional

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 import AppSettings from '../AppSettings'
 import PropTypes from 'prop-types'
 
-class BookShelf extends Component {   
-    static propTypes ={
-        shelfInfo:PropTypes.object.isRequired,
-        onShelfToggled: PropTypes.func
-    }
+function BookShelf(props) {
 
-    toggleTitle=(name)=>{
-        const {onShelfToggled} = this.props;
+   const toggleTitle= (name)=>{
+        const { onShelfToggled } = props;
         onShelfToggled && onShelfToggled(name);
     }
 
+    const { shelfInfo, collapsed, onShelfChanged } = props
+    const books = shelfInfo.Rollup.books;
+    books.sort((a, b) => a.title.localeCompare(b.title));
     
-    render() {
-        const { shelfInfo,collapsed,onShelfChanged} = this.props                
-        const books = shelfInfo.Rollup.books;                
-        books.sort((a,b)=>a.title.localeCompare(b.title));
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title" onClick={()=>this.toggleTitle(shelfInfo.Fact)}>
-                    <i className="fa fa-caret-right bookshelf-title-button" aria-hidden="true" data-collapsed={collapsed}></i> {`${AppSettings.nameMap[shelfInfo.Fact]} (${books.length})`}</h2>
-                <div className="bookshelf-books" style={{display:collapsed?"none":"initial"}}>
-                    <ol className="books-grid">
-                        {
-                            books.map(b => <li key={b.id}><Book onShelfChanged={onShelfChanged} book={b}/></li>)
-                        }
-                    </ol>
-                </div>
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title" onClick={() => toggleTitle(shelfInfo.Fact)}>
+                <i className="fa fa-caret-right bookshelf-title-button" aria-hidden="true" data-collapsed={collapsed}></i> {`${AppSettings.nameMap[shelfInfo.Fact]} (${books.length})`}</h2>
+            <div className="bookshelf-books" style={{ display: collapsed ? "none" : "initial" }}>
+                <ol className="books-grid">
+                    {
+                        books.map(b => <li key={b.id}><Book onShelfChanged={onShelfChanged} book={b} /></li>)
+                    }
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
+}
+BookShelf.propTypes = {
+    shelfInfo: PropTypes.object.isRequired,
+    onShelfToggled: PropTypes.func
 }
 
 export default BookShelf
