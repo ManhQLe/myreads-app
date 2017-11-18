d4ffa721d8675ea63d1d6657ad5534ed04ef2035
Manh Le <quang.manh@gmail.com>
2017-11-17 19:57:44 -0600

    Add debounce to input onchange event

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react'
 import Book from './Book'
+import { Debounce } from 'react-throttle'
 
 class SearchPage extends Component {
 
-    onShelfChanged = (book,shelfName)=>{
-        const {onShelfChanged} = this.props;
-        onShelfChanged &&onShelfChanged (book,shelfName)
+    onShelfChanged = (book, shelfName) => {
+        const { onShelfChanged } = this.props;
+        onShelfChanged && onShelfChanged(book, shelfName)
     }
 
     render() {
-        const {words,found} = this.props.searchInfo;
-      
+        const { words, found } = this.props.searchInfo;
+
         return (
-            <div className="search-books">                
+            <div className="search-books">
                 {
-                    found && <h1 className="search-result-text">{`Found ${found.length} book${found.length?'s':''}.`}</h1>
+                    found && <h1 className="search-result-text">{`Found ${found.length} book${found.length ? 's' : ''}.`}</h1>
                 }
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={()=>window.history.back()}>Close</a>
+                    <a className="close-search" onClick={() => window.history.back()}>Close</a>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" defaultValue={words}
-                            onChange={(e) => this.props.onQueryChanged(e.target.value)} />
+                        <Debounce time="400" handler="onChange">
+                            <input type="text" placeholder="Search by title or author" defaultValue={words}
+                                onChange={(e) => this.props.onQueryChanged(e.target.value)} />
+                        </Debounce>
                     </div>
                 </div>
                 <div className="search-books-results">
